7f24ae4bfbdf67632e0d41bf7b2096cea023908c
John Korzhuk <jkorzhuk@gmail.com>
2017-06-29 13:02:35 -0400

    init commit

---
+++ js/index.js
@@ -0,0 +1,18 @@
+import React from 'react';
+import { render } from 'react-dom';
+
+const App = () => {
+  return (
+    <div className="app">
+      <div className="landing">
+        <h1>test</h1>
+        <input type="text" placeholder="Search" />
+        <a>or Browse All</a>
+      </div>
+    </div>
+  );
+};
+
+render(<App />, document.getElementById('app'));
+
+export default App;
